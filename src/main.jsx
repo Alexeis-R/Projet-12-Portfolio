48fec9b7d5e4e87942d85e2baff01a6b6cbc98c4
Alexeis-R <alexeis.rocha1@icloud.com>
2025-03-17 01:40:27 +0100

    suppression du composant ThemeToggle

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router";
 import "./main.scss";
-import ThemeToggle from "./components/toogle_theme"; // Import du composant ThemeToggleÒ
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div>
-      <ThemeToggle /> {/* Ajout du composant ThemeToggle */}
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>
