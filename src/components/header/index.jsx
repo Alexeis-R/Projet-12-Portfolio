4b4a7639600ee30a40e98ccb3385be9b43260455
Alexeis-R <alexeis.rocha1@icloud.com>
2025-03-17 01:44:39 +0100

    ajout du themetoggle

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ContactUs from "../contact/ContactUs";
+import ThemeToggle from "../toogle_theme/index";
 
 function Header() {
   const [showContactForm, setShowContactForm] = useState(false);
@@ -17,12 +18,13 @@ function Header() {
       <nav>
         <h1>Alexeis ROCHA</h1>
         <ul>
-          <li><a href="#">Home</a></li>
+          {/* <li><a href="#">Home</a></li> */}
           <li><a href="#skills">Skills</a></li>
-          <li><a href="#">Portfolio</a></li>
+          <li><a href="#projets">Projets</a></li>
           <li><a href="#" onClick={handleContactClick}>Contact</a></li>
           <li><a href="#about">About</a></li>
         </ul>
+        <ThemeToggle />
       </nav>
       <ContactUs isOpen={showContactForm} onRequestClose={closeModal} />
     </header>
